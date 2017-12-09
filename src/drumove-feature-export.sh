644b418cbd654166044a3d2b408269b29338e475
kaanklky <kaanklky@gmail.com>
2017-12-09 21:03:17 +0300

    Feature export archive file's name fix

--- src/drumove-feature-export.sh
+++ src/drumove-feature-export.sh
@@ -4,6 +4,6 @@ feature=$1
 
 echo "Exporting $feature feature..."
 drush fe $feature $feature*
-tar cf feature.tar.gz modules/custom/$feature
+tar cf $feature.tar.gz modules/custom/$feature
 rm -rf modules/custom/$feature
 echo "'$feature' feature archived as feature.tar.gz"
