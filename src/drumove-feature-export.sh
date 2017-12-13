9f5a081ba70a6f916309c293000b8886344acff3
kaanklky <kaanklky@gmail.com>
2017-12-14 00:39:05 +0300

    set scripts as executable & trying to import feature without any extra process via ui

--- src/drumove-feature-export.sh
+++ src/drumove-feature-export.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
 feature=$1
+timestamp=`date +%s`
 
 echo ""
 echo "Exporting $feature feature..."
-drush fe $feature $feature*
-tar cf $feature.tar.gz modules/custom/$feature
-rm -rf modules/custom/$feature
-echo "'$feature' feature archived as $feature.tar.gz"
+drush fe $feature $feature* -y
+echo "Customizing '$feature' to avoid conflicts in installation"
+echo ""
+sed -i '1d' modules/custom/$feature/$feature.info.yml
+sed -i "1s/^/name: $feature $timestamp\n/" modules/custom/$feature/$feature.info.yml
+mv modules/custom/$feature/$feature.features.yml modules/custom/$feature/$feature_$timestamp.features.yml
+mv modules/custom/$feature/$feature.info.yml modules/custom/$feature/$feature_$timestamp.info.yml
+mv modules/custom/$feature modules/custom/$feature_$timestamp
+echo ""
+echo "Customization completed"
+tar cf $feature_$timestamp.tar.gz modules/custom/$feature_$timestamp
+rm -rf modules/custom/$feature_$timestamp
+echo "'$feature' feature archived as $feature_$timestamp.tar.gz"
 echo ""
--- src/drumove-feature-import.sh
+++ src/drumove-feature-import.sh
@@ -7,10 +7,27 @@ if [[ ! -f "$feature.tar.gz" ]]
 then
   echo "Missing $feature.tar.gz!"
 else
+  echo "Controlling drush tool..."
+  if [[ ! -f "./drush" ]]
+  then
+    echo "Cannot find drush, I'm downloading it..."
+    wget https://github.com/drush-ops/drush/releases/download/8.1.15/drush.phar
+    chmod +x drush.phar
+    mv drush.phar drush
+  fi
   echo "Importing $feature.tar.gz"
   echo ""
   tar xvf $feature.tar.gz
   echo ""
   echo "$feature imported."
+  echo "Enabling $feature..."
+  echo ""
+  ./drush en $feature -y
+  echo ""
+  echo "Writing $feature..."
+  echo ""
+  ./drush fim $feature --force -y
+  echo ""
+  echo "I think installation completed! Woo hoo..."
 fi
 echo ""
